4b4cd3e0ffcf9c78afc961b09eae0d50b4e85f24
LaurGen <oprealaur@yahoo.com>
2022-12-27 00:12:21 +0200

    Exit ingame menu , adjust sounds

---
+++ NPSB/Assets/Scripts/ExitIngameMenu.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitIngameMenu : MonoBehaviour
+{
+    public GameObject exitMenu;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        exitMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (Globals.isPausedExit)
+            {
+                ResumeGame();
+            }
+            else { PauseGame(); }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        if (!Globals.isPaused)
+        {
+            Globals.freezeMovement = false;
+            Globals.freezeDrunkenness = false;
+            Globals.freezeInteractions = false;
+        }
+        Globals.isPausedExit = false;
+        exitMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void PauseGame()
+    {
+        exitMenu.SetActive(true);
+        Globals.freezeMovement = true;
+        Globals.freezeDrunkenness = true;
+        Globals.freezeInteractions = true;
+        Globals.isPausedExit = true;
+        Time.timeScale = 0f;
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f; 
+        Globals.freezeMovement = false;
+        Globals.freezeDrunkenness = false;
+        Globals.freezeInteractions = false;
+        Globals.isPausedExit = false;
+        Globals.isPaused = false;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
